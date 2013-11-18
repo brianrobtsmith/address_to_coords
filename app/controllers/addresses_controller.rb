eeac53dd95e6f860b923400c298a9350bb4418fd
Brian Smith <smith.brianr@gmail.com>
2013-11-17 19:21:42 -0600

    Successfully entered new address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
+    @address = params[:location]
     @url_safe_address = URI.encode(@address)
     @url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + (@url_safe_address) + "&sensor=false"
     @raw_data = open(@url).read
@@ -11,4 +11,8 @@ class AddressesController < ApplicationController
     @latitude = @parsed_data["results"][0]["geometry"]["location"]["lat"]
     @longitude = @parsed_data["results"][0]["geometry"]["location"]["lng"]
   end
+
+  def input
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 AddressToCoords::Application.routes.draw do
   get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/address", { :controller => "addresses", :action => "input" })
 end
